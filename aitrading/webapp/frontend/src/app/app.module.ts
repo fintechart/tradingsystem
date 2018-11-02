360891490b103a88cf10495cb3cdae45f1262375
finnshen <brightcrest@gmail.com>
2018-11-02 16:14:05 +0800

    commit

--- aitrading/webapp/frontend/src/app/app.module.ts
+++ aitrading/webapp/frontend/src/app/app.module.ts
@@ -1,20 +1,53 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-import {HttpClientModule} from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
 
-import {AppComponent} from './app.component';
-import {ExamsApiService} from './exams/exams-api.service';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { AuthService } from './services/auth.service';
+import { RegisterComponent } from './components/register/register.component';
+import { StatusComponent } from './components/status/status.component';
+import { EnsureAuthenticated } from './services/ensure-authenticated.service';
+import { LoginRedirect } from './services/login-redirect.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    StatusComponent,
   ],
   imports: [
     BrowserModule,
-    HttpClientModule,
+    HttpModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'login',
+        component: LoginComponent,
+        canActivate: [LoginRedirect]
+      },
+      {
+        path: 'register',
+        component: RegisterComponent,
+        canActivate: [LoginRedirect]
+      },
+      {
+        path: 'status',
+        component: StatusComponent,
+        canActivate:
+        [EnsureAuthenticated]
+      }
+    ])
   ],
-  providers: [ExamsApiService],
+  providers: [
+    AuthService,
+    EnsureAuthenticated,
+    LoginRedirect
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule {
-}
+
+export class AppModule { }
